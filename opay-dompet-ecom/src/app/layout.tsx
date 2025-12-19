368f0afeba2e9cdf8c5cd3c2e5c2e2895d5f4804
mrssn02 <sonandra111@gmail.com>
2025-12-19 20:26:55 +0700

    Update layout.tsx

--- opay-dompet-ecom/src/app/layout.tsx
+++ opay-dompet-ecom/src/app/layout.tsx
@@ -1,20 +1,17 @@
-import "./globals.css";
-import { Navbar } from "@/components/Navbar";
-import { WhatsAppCSButton } from "@/components/WhatsAppCSButton"
+import './globals.css'
+import WhatsAppCSButton from '@/components/WhatsAppCSButton'
 
-export const metadata = {
-  title: process.env.NEXT_PUBLIC_APP_NAME || "O-Pay",
-  description: "E-commerce + Dompet Digital (demo)",
-};
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="id">
       <body>
-        <Navbar />
-        <main className="mx-auto max-w-5xl px-4 py-8">{children}</main>
+        {children}
         <WhatsAppCSButton />
       </body>
     </html>
-  );
+  )
 }
