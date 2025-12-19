f7bf1911b180b4bca7e6a707f16beff37639e17a
mrssn02 <sonandra111@gmail.com>
2025-12-19 20:02:43 +0700

    Update WhatsAppCSButton.tsx

--- opay-dompet-ecom/src/components/WhatsAppCSButton.tsx
+++ opay-dompet-ecom/src/components/WhatsAppCSButton.tsx
@@ -14,6 +14,7 @@ export default function WhatsAppCSButton() {
     <a
       href={`https://wa.me/${data.value}`}
       target="_blank"
+      rel="noopener noreferrer"
       className="fixed bottom-6 right-6 bg-green-500 text-white px-4 py-3 rounded-full shadow-lg"
     >
       CS WhatsApp
